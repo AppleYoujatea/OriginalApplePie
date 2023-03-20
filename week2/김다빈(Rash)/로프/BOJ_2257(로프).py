4167f399580a27e78775307d831865c31be2e2df
kdb1217 <mokdb17@gmail.com>
2023-03-20 22:21:22 +0900

    [월요일] 로프문제 제출

--- week2/김다빈(Rash)/로프/BOJ_2257(로프).py
+++ week2/김다빈(Rash)/로프/BOJ_2257(로프).py
@@ -1,22 +1,18 @@
-#
-# count = int(input())
-# lope_weight = []
-# sum = 0
-# for _ in range(count):
-#     lope_weight.append(int(input()))
-# lope_weight.sort()
-# for i in range(len(lope_weight)):
-#     if lope_weight[i] * len(lope_weight) < lope_weight[i + 1] * (len(lope_weight) - 1) :
-#         lope_weight.remove(lope_weight[i])
-#         sum = lope_weight[i] * len(lope_weight)
-#     elif:
-#         lope_weight.count(lope_weight[i] * len(lope_weight) > lope_weight[-1]
-#         sum = lope_weight * (int)len(lope_weight)
-#
-#     else:
-#         sum = lope_weight[i] * len(lope_weight)
-#         break
-#
-# print(sum)
+import sys
+count = int(sys.stdin.readline())
+lope_list = []
+for i in range(count):
+    lope_list.append(int(input()))
+lope_list.sort()
 
-## 꼭 풀고 말꺼야
+res = 0
+king = []
+
+for i in range(len(lope_list)):
+    king.append(lope_list[i] * count)
+    count -= 1
+
+res = max(king)
+
+
+print(res)
