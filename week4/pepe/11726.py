6498a5f79d662f82206e8a40cc7c4b89322c39fa
kimpepe <unboxing96@gmail.com>
2023-03-29 23:41:40 +0900

    [PEPE] 2n 타일링

---
+++ week4/pepe/11726.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+dp = [0] * (n + 1)
+dp[1] = 1
+dp[2] = 2
+
+def fibo(x):
+    for i in range(3, n+1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[x]
+
+print(fibo(n) % 10007)
