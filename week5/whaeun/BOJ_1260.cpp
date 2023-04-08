2647c348394b4430b3742736661ed2858ff24287
whaeun25 <61380136+whaeundo25@users.noreply.github.com>
2023-04-08 16:19:28 +0900

    DFS와 BFS 제출

---
+++ week5/whaeun/BOJ_1260.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+vector<vector<int>> edge;
+vector<int> num;
+vector<int> visited;
+
+void dfs(int node){
+    cout << node << " ";
+    visited[node] = true;
+    
+    for(int i = 0; i < edge[node].size(); i++){
+        int next = edge[node][i];
+        
+        if(!visited[next]){
+            visited[next] = true;
+            dfs(next);
+        }
+    }
+}
+
+void bfs(int node){
+    queue<int> q;
+    
+    q.push(node);
+    visited[node] = true;
+    
+    while(!q.empty()){
+        int now = q.front();
+        cout << now << " ";
+
+        q.pop();
+        
+        for(int i = 0;  i < edge[now].size(); i++){
+            if(!visited[edge[now][i]]){
+                q.push(edge[now][i]);
+                visited[edge[now][i]] = true;
+            }
+            
+            
+        }
+        
+    }
+}
+
+
+int main(){
+    
+    int n, m, v;
+    
+    cin >> n >> m >> v;
+    
+    for(int i = 0; i <= n; i++){
+        edge.push_back(num);
+        visited.push_back(0);
+    }
+    
+    for(int i = 0; i < m; i++){
+        int a, b;
+        
+        cin >> a >> b;
+        
+        edge[a].push_back(b);
+        edge[b].push_back(a);
+    }
+    
+    for(int i = 0; i <= n; i++){
+        sort(edge[i].begin(), edge[i].end());
+    }
+    
+    dfs(v);
+    cout << "\n";
+    
+    for(int i = 0; i <= n; i++){
+        visited[i] = false;
+    }
+    
+    bfs(v);
+}
