3749aa18f42cdb7fdb11b3d59bb4b327c4becacc
yunwkgus <52576276+yunwkgus@users.noreply.github.com>
2023-06-27 03:26:09 +0900

    Update 덧칠하기.py

--- 2nd_quarter/week02/Jose/덧칠하기.py
+++ 2nd_quarter/week02/Jose/덧칠하기.py
@@ -1,1 +1,16 @@
 
+def solution(n, m, section):
+
+    cnt = 0
+    idx = 0 
+    r = 0 
+    
+    # 깨달음 ! 시작을 0부터 할 필요가 없다는 사실을 
+    while idx < len(section):
+        # print(r, section[idx])
+        if r < section[idx]:
+            r = section[idx] + m - 1
+            cnt +=1
+        idx += 1 
+
+    return cnt
