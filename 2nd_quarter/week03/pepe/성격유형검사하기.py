d919e5b119b32013f506b553900203d041e05480
kimpepe <unboxing96@gmail.com>
2023-07-04 01:16:58 +0900

    PEPE 성격유형검사하기

--- 2nd_quarter/week03/pepe/성격유형검사하기.py
+++ 2nd_quarter/week03/pepe/성격유형검사하기.py
@@ -7,22 +7,31 @@ survey 리스트 : [성격 유형 2가지]
 """
 
 def solution(survey, choices):
-    answer = ''
-    dic= {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
-    
-    for s,c in zip(survey,choices):
-        if c>4:     dic[s[1]] += c-4
-        elif c<4:   dic[s[0]] += 4-c
-    
-    li = list(dic.items())
+    # 성격 유형별 점수를 저장할 딕셔너리 초기화
+    scores = {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
 
-    for i in range(0, len(li), 2):
-        if li[i][1] < li[i+1][1]:
-            answer += li[i+1][0]
+    # 각 설문 조사의 응답에 따라 해당하는 성격 유형에 점수 추가
+    for pers, choice in zip(survey,choices):
+        if choice > 4:     
+            scores[pers[1]] += choice - 4
+        elif choice < 4:   
+            scores[pers[0]] += 4 - choice
+
+    # 결과 문자열 초기화
+    result = ''
+
+    # 성격 유형별로 점수를 비교하고, 더 높은 점수를 가진 성격 유형을 결과 문자열에 추가
+    # 같은 점수라면 사전 순서가 빠른 것을 추가
+    pers_types = list(scores.items())
+
+    for i in range(0, len(pers_types), 2):
+        if pers_types[i][1] < pers_types[i+1][1]:
+            result += pers_types[i+1][0]
         else:
-            answer += li[i][0]
+            result += pers_types[i][0]
     
-    return answer
+    return result
+
 
 survey = ["AN", "CF", "MJ", "RT", "NA"]
 choices = [5, 3, 2, 7, 5]
